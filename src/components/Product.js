90b66dc41be070583210cc83e88a13d6d682c443
Michal Koudelka <koudelka.michal@gmail.com>
2019-04-15 20:41:18 +0200

    minor styling improvements

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,13 +9,13 @@ class Product extends React.Component {
   render() {
      return (
       <div className="productComponent">
-        <div className="productInfo">
-          <img src={`/assets/${this.props.id}.png`} alt={this.props.title} />
-          <h2>{this.props.title}</h2>
+        <div className="productImage">
+          <img src={`/assets/${this.props.id}.png`} alt={this.props.title} /> 
+          <div className="cartOverlay">
+            <button onClick={() => {this.props.cart.addProduct(this.props.id)}}>Add to Cart</button>
+          </div>
         </div>
-        <div className="cartOverlay">
-          <button onClick={() => {this.props.cart.addProduct(this.props.id)}}>Add to Cart</button>
-        </div>
+        <h2>{this.props.title}</h2>
       </div>
     )   
   }
